c0daa6740293745b22aa91d7350e8718cc8e8fab
stiven salazar <stivensalazar1989@gmail.com>
2025-09-25 19:17:01 -0500

    Update Consulta #2.sql

--- Consulta #2.sql
+++ Consulta #2.sql
@@ -12,25 +12,7 @@ telefono VARCHAR (10) NOT NULL,
 correo_electronico VARCHAR (100)UNIQUE NOT NULL
 );
 
-
-
 DESCRIBE visitantes;
-
-
-
-CREATE TABLE entradas(
-id INT AUTO_INCREMENT PRIMARY KEY,
-fecha DATE NOT NULL DEFAULT CURRENT_DATE,
-tipo VARCHAR (20) NOT NULL,
-precio DECIMAL (10,2) NOT NULL CHECK (precio>=0),
-DNI_visitante VARCHAR(15)  NOT NULL,
-FOREIGN KEY (DNI_visitante) REFERENCES visitantes(DNI) 
-);
-
-
-
-
-DESCRIBE entradas;
 
 
 CREATE TABLE atracciones(
@@ -63,9 +45,22 @@ nombre_cargo VARCHAR (20) UNIQUE NOT NULL,
 PRIMARY KEY (id_cargo)
 );
 
-DROP TABLE cargos;
+
 DESCRIBE cargos;
 
+
+
+CREATE TABLE entradas(
+id INT AUTO_INCREMENT PRIMARY KEY,
+fecha DATE NOT NULL DEFAULT CURRENT_DATE,
+tipo VARCHAR (20) NOT NULL,
+precio DECIMAL (10,2) NOT NULL CHECK (precio>=0),
+DNI_visitante VARCHAR(15)  NOT NULL,
+FOREIGN KEY (DNI_visitante) REFERENCES visitantes(DNI) 
+);
+
+
+DESCRIBE entradas;
 
 
 
@@ -75,7 +70,7 @@ nombre VARCHAR (30) NOT NULL,
 id_cargo INT,
 PRIMARY KEY (id)
 );
-DROP TABLE empleados;
+
 ALTER TABLE empleados
 ADD CONSTRAINT FK_cargoempleado
 FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo);
@@ -88,5 +83,6 @@ DESCRIBE atracciones;
 DESCRIBE acceso_atracciones;
 DESCRIBE cargos;
 DESCRIBE empleados;
+
 
 
